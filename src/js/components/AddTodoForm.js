9d3067ae3cea956aa82dee8cc00d1a6c57f1d7ca
Johnny Choo <jchoo156@gmail.com>
2018-06-16 02:32:28 -0700

    add scroll to node function - test

--- src/js/components/AddTodoForm.js
+++ src/js/components/AddTodoForm.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 
 export default class AddTodo extends Component {
+  scrollToInput(node) {
+    node.scrollIntoView()
+  }
+
   render() {
     const {todo, submitTodo, updateTodo, deleteTodo} = this.props
     let submitMethod = todo.editing ? updateTodo : submitTodo
     return(
       <div className="todo-form vertical-align">
-        <form className={todo.text ? 'pad-top-bot' : ''} onSubmit={e => {e.preventDefault(); submitMethod(todo.id, this.input.value)}}>
-          <input ref={node => {this.input = node}} className="task-input" placeholder="Enter task name..." defaultValue={todo.text} required={true} autoFocus={true}/>
+        <form className={todo.text ? 'pad-top-bot' : ''} onSubmit={e => {e.preventDefault(); submitMethod(todo.id, this.input.value); this.scrollToInput(this.input)}}>
+          <input ref={node => {this.input = node}} id="type-task" className="task-input" placeholder="Enter task name..." defaultValue={todo.text} required={true} autoFocus={true}/>
           <div className="edit-options">
             <button className="task-save-btn" type="submit">Save</button>
             {!todo.text ? <button className="cancel-todo" onClick={deleteTodo}>Cancel</button> : null}
