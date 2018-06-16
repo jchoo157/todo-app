1eed636af0c19bc9c7013843c6e4b4bf60ad61ff
Johnny Choo <jchoo156@gmail.com>
2018-06-16 02:55:58 -0700

    undo changes

--- src/js/components/AddTodoForm.js
+++ src/js/components/AddTodoForm.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
 
 export default class AddTodo extends Component {
-  scrollToInput(e) {
-    e.target.scrollIntoView()
-  }
-
   render() {
     const {todo, submitTodo, updateTodo, deleteTodo} = this.props
     let submitMethod = todo.editing ? updateTodo : submitTodo
     return(
       <div className="todo-form vertical-align">
-        <form className={todo.text ? 'pad-top-bot' : ''} onSubmit={e => {e.preventDefault(); submitMethod(todo.id, this.input.value);}} onFocus={(e) => this.scrollToInput(e)}>
+        <form className={todo.text ? 'pad-top-bot' : ''} onSubmit={e => {e.preventDefault(); submitMethod(todo.id, this.input.value)}}>
           <input ref={node => {this.input = node}} className="task-input" placeholder="Enter task name..." defaultValue={todo.text} required={true} autoFocus={true}/>
           <div className="edit-options">
             <button className="task-save-btn" type="submit">Save</button>
