39ff0733cd03bbe8bb3f3ff9e478f5fc8f306107
Johnny Choo <jchoo156@gmail.com>
2018-06-16 02:45:14 -0700

    try another way to move to focused input

--- src/js/components/AddTodoForm.js
+++ src/js/components/AddTodoForm.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 
 export default class AddTodo extends Component {
-  scrollToInput(node) {
-    node.scrollIntoView()
+  scrollToInput(e) {
+    e.target.scrollIntoView()
   }
 
   render() {
@@ -10,8 +10,8 @@ export default class AddTodo extends Component {
     let submitMethod = todo.editing ? updateTodo : submitTodo
     return(
       <div className="todo-form vertical-align">
-        <form className={todo.text ? 'pad-top-bot' : ''} onSubmit={e => {e.preventDefault(); submitMethod(todo.id, this.input.value); this.scrollToInput(this.input)}}>
-          <input ref={node => {this.input = node}} id="type-task" className="task-input" placeholder="Enter task name..." defaultValue={todo.text} required={true} autoFocus={true}/>
+        <form className={todo.text ? 'pad-top-bot' : ''} onSubmit={e => {e.preventDefault(); submitMethod(todo.id, this.input.value);}} onFocus={(e) => this.scrollToInput(e)}>
+          <input ref={node => {this.input = node}} className="task-input" placeholder="Enter task name..." defaultValue={todo.text} required={true} autoFocus={true}/>
           <div className="edit-options">
             <button className="task-save-btn" type="submit">Save</button>
             {!todo.text ? <button className="cancel-todo" onClick={deleteTodo}>Cancel</button> : null}
