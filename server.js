f49a8c4435a33123492ff6e6ad39d63a604a74d5
Johnny Choo <jchoo156@gmail.com>
2018-06-16 23:34:18 -0700

    add trick to prevent server idling in heroku

--- server.js
+++ server.js
@@ -9,5 +9,9 @@ app.get('*', (req, res) => {
   res.send(path.resolve(__dirname, '/dist/index.html'))
 });
 
+setInterval(function() {
+  app.get("http://simple-task-todo.herokuapp.com");
+}, 300000);
+
 app.listen(port);
 console.log('Server started');
